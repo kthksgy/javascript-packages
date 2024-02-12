57841597dc8437601e7e760f98119744c96da09b
kthksgy <1616191+kthksgy@users.noreply.github.com>
2024-02-12 22:27:40 +0900

    プロジェクトのテンプレートをコピー

---
+++ lint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ue
+# ドライランコンパイル対象のディレクトリ名
+DIRECTORY_NAMES=('string-template')
+
+# `packages`ディレクトリを指定された順でドライランコンパイルする。
+for DIRECTORY_NAME in ${DIRECTORY_NAMES[@]};
+do
+  echo "\"${DIRECTORY_NAME}\"パッケージのコンパイルを行います。"
+  (cd packages/${DIRECTORY_NAME} && tsc --noEmit)
+done
+
+echo "ESLintを実行します。"
+eslint .
