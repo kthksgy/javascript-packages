a5e8de29fd35e600573a36d33f92a803cebb517a
kthksgy <1616191+kthksgy@users.noreply.github.com>
2024-03-08 11:41:06 +0900

    `procrastinate`関数のテストを追加

--- packages/common/src/functions/procrastinate.test.ts
+++ packages/common/src/functions/procrastinate.test.ts
@@ -1,5 +1,24 @@
 import { expect, test } from 'vitest';
 
-test('TODO', function () {
-  expect(1).toBe(1);
+import { procrastinate } from './procrastinate';
+
+test('最初に作成した値を使い回している', function () {
+  function normal() {
+    return {};
+  }
+  expect(normal()).not.toBe(normal());
+
+  const procrastinated = procrastinate(normal);
+  expect(procrastinated()).toBe(procrastinated());
+});
+
+test('実行されるまで値が作成されない', function () {
+  let called = false;
+  const procrastinated = procrastinate(function () {
+    called = true;
+    return {};
+  });
+  expect(called).toBe(false);
+  procrastinated();
+  expect(called).toBe(true);
 });
