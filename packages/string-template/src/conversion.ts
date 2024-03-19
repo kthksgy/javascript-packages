397b753b25f5c45cca6b76c7b352ba7389ed73d3
kthksgy <1616191+kthksgy@users.noreply.github.com>
2024-03-19 13:27:08 +0900

    パステンプレートを文字列テンプレートに変換する関数を汎化

--- packages/string-template/src/conversion.ts
+++ packages/string-template/src/conversion.ts
@@ -1,75 +1,81 @@
-export type ConvertStringTemplatePatternA<T extends string> = ConvertStringTemplatePatternPath<
-  T,
-  '$'
->;
-
-export type ConvertStringTemplatePatternB<T extends string> = ConvertStringTemplatePatternPath<
-  T,
-  ':'
->;
-
-export type ConvertStringTemplatePatternPath<
+/**
+ * `convertPathTemplateIntoStringTemplate`関数のアウトプット
+ */
+export type ConvertPathTemplateIntoStringTemplateOutput<
   T extends string,
   KeyPrefix extends string,
-> = T extends `${KeyPrefix}${infer Key}/${infer Suffix}`
-  ? `%${Key}%/${ConvertStringTemplatePatternPath<Suffix, KeyPrefix>}`
-  : T extends `${infer Prefix}/${KeyPrefix}${infer Key}/${infer Suffix}`
-    ? `${Prefix}/%${Key}%/${ConvertStringTemplatePatternPath<Suffix, KeyPrefix>}`
-    : T extends `${infer Prefix}/${KeyPrefix}${infer Key}`
-      ? `${Prefix}/%${Key}%`
-      : T;
+  KeySuffix extends string,
+> = T extends `${infer Former}/${infer Latter}`
+  ? `${ConvertPathTemplateIntoStringTemplateOutput<Former, KeyPrefix, KeySuffix>}/${ConvertPathTemplateIntoStringTemplateOutput<Latter, KeyPrefix, KeySuffix>}`
+  : T extends `${KeyPrefix}${infer Key}${KeySuffix}`
+    ? `%${Key}%`
+    : T;
+
+/** 正規表現の特殊文字の正規表現 */
+const specialCharacterRegularExpression = /[$()*+.?[\\\]^{|}]/g;
+
+/** パステンプレートキーの正規表現 */
+const pathTemplateKeyRegularExpressions = new Map<`${string} ${string}`, RegExp>();
 
 /**
- * 文字列テンプレートを変換する。
- *
- * @example
- * ```typescript
- * // `/users/%userId%/articles/%articleId%`
- * convertStringTemplate('a', '/users/$userId/articles/$articleId');
- * ```
+ * パステンプレートキーの正規表現を作成する。
+ * @param keyPrefix キーのプレフィックス
+ * @param keySuffix キーのサフィックス
+ * @returns 正規表現
  */
-export function convertStringTemplate<T extends string>(
-  template: T,
-  pattern: 'A',
-): ConvertStringTemplatePatternA<T>;
-
-/**
- * 文字列テンプレートを変換する。
- *
- * @example
- * ```typescript
- * // `/users/%userId%/articles/%articleId%`
- * convertStringTemplate('a', '/users/$userId/articles/$articleId');
- * ```
- */
-export function convertStringTemplate<T extends string>(
-  template: T,
-  pattern: 'B',
-): ConvertStringTemplatePatternB<T>;
-
-/**
- * 文字列テンプレートを
- * @param pattern 元の文字列テンプレートのパターン
- * @param template 文字列テンプレート
- * @returns 文字列テンプレート
- */
-export function convertStringTemplate<T extends string>(template: T, pattern: 'A' | 'B') {
-  switch (pattern) {
-    case 'A':
-      return template.replace(
-        /\$([^/]*)/g,
-        convertStringTemplateReplacer,
-      ) as ConvertStringTemplatePatternA<T>;
-    case 'B':
-      return template.replace(
-        /:([^/]*)/g,
-        convertStringTemplateReplacer,
-      ) as ConvertStringTemplatePatternA<T>;
-    default:
-      throw new Error(`Unknown string template pattern: "${pattern}"`);
+export function createPathTemplateKeyRegularExpression<
+  KeyPrefix extends string,
+  KeySuffix extends string = '',
+>(keyPrefix: KeyPrefix, keySuffix: KeySuffix = <KeySuffix>'') {
+  const key: `${string} ${string}` = `${keyPrefix} ${keySuffix}`;
+  let pathTemplateKeyRegularExpression = pathTemplateKeyRegularExpressions.get(key);
+  if (!pathTemplateKeyRegularExpression) {
+    pathTemplateKeyRegularExpression = new RegExp(
+      '(?<=^|/)' +
+        keyPrefix.replace(specialCharacterRegularExpression, '\\$&') +
+        '([^/]*)' +
+        keySuffix.replace(specialCharacterRegularExpression, '\\$&'),
+      'g',
+    );
+    pathTemplateKeyRegularExpressions.set(key, pathTemplateKeyRegularExpression);
   }
+  return pathTemplateKeyRegularExpression;
 }
 
-function convertStringTemplateReplacer(_: any, key: string) {
+/**
+ * パステンプレートを文字列テンプレートに変換する。
+ * @param template パステンプレート(例: `/a/$b/c/$d`, `/a/:b:/c/:d:`)
+ * @param keyPrefix キーのプレフィックス(例: `$`, `:`)
+ * @param keySuffix キーのサフィックス(例: 空文字, `:`)
+ * @returns 文字列テンプレート(例: `/a/%b%/c/%d%`, `/a/%b%/c/%d%`)
+ *
+ * @example
+ * ```typescript
+ * // `/users/%userId%/articles/%articleId%`
+ * convertPathTemplateIntoStringTemplate(
+ *   '/users/$userId/articles/$articleId', '$',
+ * );
+ * ```
+ *
+ * @example
+ * ```typescript
+ * // `/users/%userId%/articles/%articleId%`
+ * convertPathTemplateIntoStringTemplate(
+ *   '/users/:userId:/articles/:articleId:', ':', ':',
+ * );
+ * ```
+ */
+export function convertPathTemplateIntoStringTemplate<
+  T extends string,
+  KeyPrefix extends string,
+  KeySuffix extends string = '',
+>(template: T, keyPrefix: KeyPrefix, keySuffix = <KeySuffix>'') {
+  return template.replace(
+    createPathTemplateKeyRegularExpression(keyPrefix, keySuffix),
+    replacer,
+  ) as ConvertPathTemplateIntoStringTemplateOutput<T, KeyPrefix, KeySuffix>;
+}
+
+function replacer(_: any, key: string) {
   return '%' + key + '%';
 }
