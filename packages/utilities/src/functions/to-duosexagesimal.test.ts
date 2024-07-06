ea8c6941a257c38f2bf50f9bd85741a2f13fe2a4
kthksgy <1616191+kthksgy@users.noreply.github.com>
2024-07-07 02:02:01 +0900

    62進数に変換する関数のテストを追加

---
+++ packages/utilities/src/functions/to-duosexagesimal.test.ts
@@ -0,0 +1,60 @@
+import { describe, expect, test } from 'vitest';
+
+import { toDuosexagesimal } from './to-duosexagesimal';
+
+describe(`${toDuosexagesimal.name}()`, function () {
+  test.each([
+    [0, '0'],
+    [1, '1'],
+    [61, 'z'],
+    [62, '10'],
+    [3843, 'zz'],
+    [3844, '100'],
+    [238327, 'zzz'],
+    [238328, '1000'],
+    [14776335, 'zzzz'],
+    [14776336, '10000'],
+    [916132831, 'zzzzz'],
+    [916132832, '100000'],
+    [56800235583, 'zzzzzz'],
+    [56800235584, '1000000'],
+    [3521614606207, 'zzzzzzz'],
+    [3521614606208, '10000000'],
+    [218340105584895, 'zzzzzzzz'],
+    [218340105584896, '100000000'],
+  ])('[%#] `%d`を`%s`に変換できる', function (decimal, duosexagesimal) {
+    expect(toDuosexagesimal(decimal)).toBe(duosexagesimal);
+  });
+
+  test('`Number.MAX_SAFE_INTEGER(9007199254740991)`を入力できる', function () {
+    expect(Number.MAX_SAFE_INTEGER).toBe(9007199254740991);
+    expect(toDuosexagesimal(Number.MAX_SAFE_INTEGER)).toBe('fFgnDxSe7');
+  });
+
+  test('0未満の整数を入力できない', function () {
+    expect(function () {
+      toDuosexagesimal(-1);
+    }).toThrowError(TypeError);
+  });
+
+  test('非整数を入力できない', function () {
+    expect(function () {
+      toDuosexagesimal(0.1);
+    }).toThrowError(TypeError);
+  });
+
+  test('`Infinity`と`-Infinity`を入力できない', function () {
+    expect(function () {
+      toDuosexagesimal(Infinity);
+    }).toThrowError(TypeError);
+    expect(function () {
+      toDuosexagesimal(-Infinity);
+    }).toThrowError(TypeError);
+  });
+
+  test('`NaN`を入力できない', function () {
+    expect(function () {
+      toDuosexagesimal(NaN);
+    }).toThrowError(TypeError);
+  });
+});
