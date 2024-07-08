d9bb9c70c47e6d69eb06685a61eb82ec13fcf404
kthksgy <1616191+kthksgy@users.noreply.github.com>
2024-07-08 17:44:29 +0900

    ミスハップのフィールドを簡略化

--- packages/mishap/src/main.ts
+++ packages/mishap/src/main.ts
@@ -31,42 +31,38 @@ function hasStack<T>(instance: T): instance is T & { stack: string } {
 }
 
 /**
+ * 属性をサニタイズして、JSONオブジェクトである事を保証する。
+ * @param attributes 属性
+ * @returns サニタイズされた属性
+ */
+function sanitizeAttributes(attributes: unknown) {
+  if (typeof attributes !== 'object' || attributes === null) {
+    return undefined;
+  }
+
+  return JSON.parse(JSON.stringify(attributes));
+}
+
+/**
  * ミスハップ
  * 通常の例外と区別するための独自の例外クラス。
  */
 export class Mishap extends Error {
-  /** キャプション */
-  caption?: string;
+  /** 属性 */
+  attributes?: MishapAttributes;
   /** コード */
   code: string;
-  /** HTTPレスポンスステータスコード */
-  http?: number;
-  /** ラベル */
-  label?: string;
-  /** パラメーター */
-  parameters?: object;
   /** タイムスタンプ */
   timestamp: Date;
 
   constructor(code: string, options?: MishapOptions) {
-    code = Mishap.buildCode(code);
-    let message: string = code;
-    if (options?.caption) {
-      message += ' ' + options.caption;
-    }
-    if (options?.parameters) {
-      message += ' ' + JSON.stringify(options.parameters);
-    }
-    super(message, options);
+    super(options?.message, options);
 
-    this.caption = options?.caption;
+    this.attributes = sanitizeAttributes(options?.attributes);
     this.code = code;
-    this.http = options?.http;
-    this.label = Mishap.buildLabel(options?.label);
-    this.parameters = options?.parameters;
     this.timestamp = options?.timestamp ?? new Date();
 
-    this.name = Mishap.name + '[' + this.code + ']';
+    this.name = 'Mishap[' + this.code + ']';
     this.stack = concatenateStacks(this, options?.cause);
   }
 
@@ -75,151 +71,107 @@ export class Mishap extends Error {
    * このインスタンスの原因となったインスタンスを祖先を先頭にして返す。
    */
   get causes() {
-    const causes = [];
+    const causes = new Set();
     let cause: any = this.cause;
-    while (cause) {
-      if (cause !== null && cause !== undefined) {
-        causes.unshift(cause);
-        if (typeof cause === 'object' && 'cause' in cause) {
-          cause = cause.cause;
-        } else {
-          cause = null;
-        }
+    while (cause !== undefined) {
+      if (causes.has(cause)) {
+        break;
+      }
+
+      causes.add(cause);
+
+      if (typeof cause === 'object' && cause !== null && 'cause' in cause) {
+        cause = cause.cause;
       } else {
         break;
       }
     }
-    return causes;
+
+    return Array.from(causes).reverse();
   }
 
-  /**
-   * ラベルを追加する。ラベルのみ異なる新しいインスタンスを返す。
-   * @param labels ラベル
-   * @returns ラベルのみ異なる新しいインスタンス
-   */
-  addLabel(...labels: Array<string>) {
-    return new Mishap(Mishap.buildCode(this.code), {
-      caption: this.caption,
-      cause: this.cause,
-      http: this.http,
-      label: Mishap.buildLabel(this.label, ...labels),
-      parameters: this.parameters,
-      timestamp: this.timestamp,
-    });
+  toJSON() {
+    return {
+      attributes: this.attributes,
+      code: this.code,
+      message: this.message,
+      timestamp: this.timestamp.toISOString(),
+    };
   }
 
   toObject() {
-    return {
-      caption: this.caption,
-      code: this.code,
-      http: this.http,
-      label: this.label,
-      parameters: this.parameters,
-      timestamp: this.timestamp.getTime(),
-    };
+    return this.toJSON();
+  }
+
+  toString() {
+    return (
+      this.name +
+      (this.message ? ': ' + this.message : '') +
+      (this.attributes ? ' ' + JSON.stringify(this.attributes) : '') +
+      ' @ ' +
+      this.timestamp.toISOString()
+    );
   }
 
   /** 既定のコード */
   static readonly DEFAULT_CODE = 'MISHAP';
-  /** ラベルの区切り文字 */
-  static readonly LABEL_DELIMITER = '!';
-  /** ラベルの接頭辞 */
-  static readonly LABEL_PREFIX = '#';
-  /** クラス名 */
-  static readonly name = 'Mishap';
 
-  /**
-   * コードを作成する。
-   * @param code 使用可能な文字のみで構成されたコード
-   * @returns コード(空文字列の場合は既定のコード)
-   */
-  static buildCode(code: string | undefined) {
-    if (!code) {
-      return Mishap.DEFAULT_CODE;
-    } else if (code.includes(Mishap.LABEL_DELIMITER) || code.includes(Mishap.LABEL_PREFIX)) {
-      console.warn(
-        `コード("${code}")は使用不可能であるため既定のコード("${Mishap.DEFAULT_CODE}")を使用します。`,
-      );
-      return Mishap.DEFAULT_CODE;
-    } else {
-      return code;
-    }
-  }
-
-  /**
-   * ラベルを作成する。
-   * @param labels ラベルの配列
-   * @returns ラベル(空文字列の場合は`undefined`)
-   */
-  static buildLabel(...labels: Array<string | undefined>) {
-    /** ラベル */
-    let label = '';
-    for (let i = 0; i < labels.length; i++) {
-      const l = labels[i];
-      if (!l) {
-        continue;
-      } else if (l.includes(Mishap.LABEL_DELIMITER)) {
-        console.warn(`${i + 1}つ目のラベル("${l}")は使用不可能であるため省略します。`);
-        continue;
-      } else {
-        label += (label.length > 0 ? Mishap.LABEL_DELIMITER : '') + l;
+  static fromObject(instance: any) {
+    try {
+      if (typeof instance !== 'object' || instance === null) {
+        throw new Mishap(Mishap.DEFAULT_CODE, {
+          cause: instance,
+          message: `${JSON.stringify(instance)} is not an object.`,
+        });
       }
-    }
-    return label || undefined;
-  }
 
-  static fromObject(instance?: Record<string, unknown>) {
-    if (
-      instance &&
-      typeof instance === 'object' &&
-      (instance.caption === undefined || typeof instance.caption === 'string') &&
-      typeof instance.code === 'string' &&
-      (instance.http === undefined || typeof instance.http === 'number') &&
-      (instance.label === undefined || typeof instance.label === 'string') &&
-      (instance.parameters === undefined ||
-        (instance.parameters !== null && typeof instance.parameters === 'object')) &&
-      typeof instance.timestamp === 'number'
-    ) {
-      return new Mishap(instance.code, {
-        caption: instance.caption,
-        http: instance.http,
-        label: instance.label,
-        parameters: instance.parameters,
-        timestamp: new Date(instance.timestamp),
-      });
-    } else {
-      return new Mishap(Mishap.DEFAULT_CODE, {
-        caption: '`fromObject`が意図しない形式です。',
-        cause: instance,
-        http: 400,
-      });
+      const attributes = sanitizeAttributes(instance.attributes);
+
+      const message = instance.message;
+      if (typeof message !== 'string') {
+        throw new Mishap(Mishap.DEFAULT_CODE, {
+          cause: instance.message,
+          message: `${JSON.stringify(message)} is not a string.`,
+        });
+      }
+
+      const timestamp = new Date(instance.timestamp);
+      if (Number.isSafeInteger(timestamp.getTime())) {
+        throw new Mishap(Mishap.DEFAULT_CODE, {
+          cause: instance.timestamp,
+          message: `${JSON.stringify(instance.timestamp)} is not a valid date.`,
+        });
+      }
+
+      return new Mishap(instance.code, { attributes, message, timestamp });
+    } catch (error) {
+      if (error instanceof Mishap) {
+        return error;
+      } else {
+        return new Mishap(Mishap.DEFAULT_CODE, {
+          cause: error,
+          message: `Mishap.fromObject(${JSON.stringify(instance)}) failed.`,
+        });
+      }
     }
   }
 }
+
+type JsonObject = { [Key in string]: JsonValue } & { [Key in string]?: JsonValue | undefined };
+type JsonArray = JsonValue[] | readonly JsonValue[];
+type JsonValue = string | number | boolean | JsonObject | JsonArray | null;
+
+/** ミスハップ属性 */
+export type MishapAttributes = Record<string, JsonValue | undefined>;
 
 /**
  * ミスハップオプション
  */
 export interface MishapOptions extends ErrorOptions {
-  /**
-   * キャプション
-   * エラーの説明をする短い文字列。
-   * 秘匿情報を含めない。
-   */
-  caption?: string;
-  /** HTTPレスポンスステータスコード */
-  http?: number;
-  /**
-   * パラメーター
-   * エラーのパラメーターオブジェクト。
-   * 秘匿情報は含めない。
-   */
-  parameters?: object;
-  /**
-   * ラベル
-   * エラーの初期化位置を埋め込むためのラベル。
-   */
-  label?: string;
+  /** 属性 */
+  attributes?: MishapAttributes;
+  /** メッセージ */
+  message?: string;
   /** タイムスタンプ */
   timestamp?: Date;
 }
