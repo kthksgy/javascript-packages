f33cfb08f29612e48ef99ed5ce9b8ed95693601e
kthksgy <1616191+kthksgy@users.noreply.github.com>
2024-07-12 16:41:35 +0900

    `isArray()`関数を追加
    標準の`Array.isArray()`は`ReadonlyArray<T>`に対して型ガードをしないので追加しました。

---
+++ packages/utilities/src/functions/is-array.ts
@@ -0,0 +1,29 @@
+/**
+ * 値が配列である場合、`true`を返す。
+ * @param array 値
+ * @returns 値が配列である場合、`true`
+ *
+ * `Array.isArray()`はランタイムでは`ReadonlyArray<T>`に対し`true`を返すが、
+ * `ReadonlyArray<T>`をTypeScript上では保証しないため、この関数を実装している。
+ * @see https://github.com/microsoft/TypeScript/issues/17002
+ * ```typescript
+ * let numbers: number | Array<number> | ReadonlyArray<number>;
+ *
+ * if (Array.isArray(numbers)) {
+ *   // Array<number>
+ * } else {
+ *   // number | ReadonlyArray<number>
+ * }
+ *
+ * if (isArray(numbers)) {
+ *  // Array<number> | ReadonlyArray<number>
+ * } else {
+ *  // number
+ * }
+ * ```
+ */
+export function isArray<T>(
+  array: T,
+): array is Extract<T extends Array<any> ? T : T extends ReadonlyArray<any> ? T : never, T> {
+  return Array.isArray(array);
+}
