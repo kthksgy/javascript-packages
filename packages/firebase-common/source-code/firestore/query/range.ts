26758a7b009e036611fd56f10d58389895060a7d
kthksgy <1616191+kthksgy@users.noreply.github.com>
2025-04-14 01:07:40 +0900

    範囲クエリパラメータのタイプを分割

--- packages/firebase-common/source-code/firestore/query/range.ts
+++ packages/firebase-common/source-code/firestore/query/range.ts
@@ -5,18 +5,22 @@ import { type DocumentData, type DocumentDataSchema, type Path, type Value } fro
 
 type AbstractDocumentSnapshot = { ref: any };
 
-export type RangeQueryParameterType = "startAfter" | "startAt" | "endBefore" | "endAt";
+export type BoundType1 = "exclusive" | "inclusive";
+export type BoundType2 = "after" | "before";
 
 export class RangeQueryParameter<
-  Type extends RangeQueryParameterType = RangeQueryParameterType,
+  Type1 extends BoundType1 = BoundType1,
+  Type2 extends BoundType2 = BoundType2,
   V = any,
 > extends QueryParameter {
   value: V;
-  readonly type: RangeQueryParameterType;
+  readonly type1: Type1;
+  readonly type2: Type2;
 
-  constructor(type: Type, value: V) {
+  constructor(type1: Type1, type2: Type2, value: V) {
     super();
-    this.type = type;
+    this.type1 = type1;
+    this.type2 = type2;
     this.value = value;
   }
 }
@@ -25,17 +29,20 @@ export class RangeQueryParameter<
  * #### ページクエリパラメータ
  */
 export class PageQueryParameter<
-  Type extends RangeQueryParameterType = RangeQueryParameterType,
+  Type1 extends BoundType1 = BoundType1,
+  Type2 extends BoundType2 = BoundType2,
 > extends QueryParameter {
   /** カーソル */
   cursor: AbstractDocumentSnapshot;
   /** タイプ */
-  readonly type: RangeQueryParameterType;
+  readonly type1: Type1;
+  readonly type2: Type2;
 
-  constructor(type: Type, cursor: AbstractDocumentSnapshot) {
+  constructor(type1: Type1, type2: Type2, cursor: AbstractDocumentSnapshot) {
     super();
     this.cursor = cursor;
-    this.type = type;
+    this.type1 = type1;
+    this.type2 = type2;
   }
 }
 
@@ -43,10 +50,10 @@ export function endBefore<
   S extends DocumentDataSchema,
   P extends Path<DocumentData<S>>,
   V extends Value<ObjectUnionIntersection<DocumentData<S>>, P>,
->(_: S | { (..._: Array<any>): S }, __: P, value: V): RangeQueryParameter<"endBefore", V>;
+>(_: S | { (..._: Array<any>): S }, __: P, value: V): RangeQueryParameter<"exclusive", "before", V>;
 export function endBefore(
   documentSnapshot: AbstractDocumentSnapshot,
-): PageQueryParameter<"endBefore">;
+): PageQueryParameter<"exclusive", "before">;
 
 export function endBefore(
   ...parameters:
@@ -54,9 +61,9 @@ export function endBefore(
     | [DocumentDataSchema | { (..._: ReadonlyArray<any>): DocumentDataSchema }, Path, any]
 ) {
   if (parameters.length === 1) {
-    return new PageQueryParameter("endBefore", parameters[0]);
+    return new PageQueryParameter("exclusive", "before", parameters[0]);
   } else {
-    return new RangeQueryParameter("endBefore", parameters[2]);
+    return new RangeQueryParameter("exclusive", "before", parameters[2]);
   }
 }
 
@@ -64,8 +71,10 @@ export function endAt<
   S extends DocumentDataSchema,
   P extends Path<DocumentData<S>>,
   V extends Value<ObjectUnionIntersection<DocumentData<S>>, P>,
->(_: S | { (..._: Array<any>): S }, __: P, value: V): RangeQueryParameter<"endAt", V>;
-export function endAt(documentSnapshot: AbstractDocumentSnapshot): PageQueryParameter<"endAt">;
+>(_: S | { (..._: Array<any>): S }, __: P, value: V): RangeQueryParameter<"inclusive", "before", V>;
+export function endAt(
+  documentSnapshot: AbstractDocumentSnapshot,
+): PageQueryParameter<"inclusive", "before">;
 
 export function endAt(
   ...parameters:
@@ -73,9 +82,9 @@ export function endAt(
     | [DocumentDataSchema | { (..._: ReadonlyArray<any>): DocumentDataSchema }, Path, any]
 ) {
   if (parameters.length === 1) {
-    return new PageQueryParameter("endAt", parameters[0]);
+    return new PageQueryParameter("inclusive", "before", parameters[0]);
   } else {
-    return new RangeQueryParameter("endAt", parameters[2]);
+    return new RangeQueryParameter("inclusive", "before", parameters[2]);
   }
 }
 
@@ -83,19 +92,19 @@ export function startAfter<
   S extends DocumentDataSchema,
   P extends Path<DocumentData<S>>,
   V extends Value<ObjectUnionIntersection<DocumentData<S>>, P>,
->(_: S | { (..._: Array<any>): S }, __: P, value: V): RangeQueryParameter<"startAfter", V>;
+>(_: S | { (..._: Array<any>): S }, __: P, value: V): RangeQueryParameter<"exclusive", "after", V>;
 export function startAfter(
   documentSnapshot: AbstractDocumentSnapshot,
-): PageQueryParameter<"startAfter">;
+): PageQueryParameter<"exclusive", "after">;
 export function startAfter(
   ...parameters:
     | [AbstractDocumentSnapshot]
     | [DocumentDataSchema | { (..._: ReadonlyArray<any>): DocumentDataSchema }, Path, any]
 ) {
   if (parameters.length === 1) {
-    return new PageQueryParameter("startAfter", parameters[0]);
+    return new PageQueryParameter("exclusive", "after", parameters[0]);
   } else {
-    return new RangeQueryParameter("startAfter", parameters[2]);
+    return new RangeQueryParameter("exclusive", "after", parameters[2]);
   }
 }
 
@@ -103,16 +112,18 @@ export function startAt<
   S extends DocumentDataSchema,
   P extends Path<DocumentData<S>>,
   V extends Value<ObjectUnionIntersection<DocumentData<S>>, P>,
->(_: S | { (..._: Array<any>): S }, __: P, value: V): RangeQueryParameter<"startAt", V>;
-export function startAt(documentSnapshot: AbstractDocumentSnapshot): PageQueryParameter<"startAt">;
+>(_: S | { (..._: Array<any>): S }, __: P, value: V): RangeQueryParameter<"inclusive", "after", V>;
+export function startAt(
+  documentSnapshot: AbstractDocumentSnapshot,
+): PageQueryParameter<"inclusive", "after">;
 export function startAt(
   ...parameters:
     | [AbstractDocumentSnapshot]
     | [DocumentDataSchema | { (..._: ReadonlyArray<any>): DocumentDataSchema }, Path, any]
 ) {
   if (parameters.length === 1) {
-    return new PageQueryParameter("startAt", parameters[0]);
+    return new PageQueryParameter("inclusive", "after", parameters[0]);
   } else {
-    return new RangeQueryParameter("startAt", parameters[2]);
+    return new RangeQueryParameter("inclusive", "after", parameters[2]);
   }
 }
